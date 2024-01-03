9fc7bcd98d4b8ded867c90b7f5b630478ef08ad4
JulianaMR22 <julianam2003@gmail.com>
2024-01-03 12:53:06 -0500

    Se realizo el responsive de la pagina web

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const containerFeactures = document.getElementById("containerFeactures");
 const feacturesMenu = document.getElementById("feacturesMenu");
 const containerCompany = document.getElementById("containerCompany");
 const companyMenu = document.getElementById("companyMenu");
+const navListMenu = document.getElementById("navListMenu");
 
 containerFeactures.addEventListener("click", () => {
   feacturesMenu.classList.toggle("menu__display-none");
 });
 containerCompany.addEventListener("click", () => {
   companyMenu.classList.toggle("menu__display-none");
+});
+navListMenu.addEventListener("click", () => {
+  navListMenu.classList.toggle("");
 });
