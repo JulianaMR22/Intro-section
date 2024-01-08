863b97fb04704d2563ea0b844d68dc94b7b04413
JulianaMR22 <julianam2003@gmail.com>
2024-01-08 10:01:09 -0500

    Se agrego funcionalidad de menu en el responsive

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const feacturesMenu = document.getElementById("feacturesMenu");
 const containerCompany = document.getElementById("containerCompany");
 const companyMenu = document.getElementById("companyMenu");
 const navListMenu = document.getElementById("navListMenu");
+const closeNav = document.getElementById("closeNav");
+const sideBar = document.getElementById("sideBar");
+const principalMenu = document.getElementById("principalMenu");
 
 containerFeactures.addEventListener("click", () => {
   feacturesMenu.classList.toggle("menu__display-none");
@@ -11,5 +14,10 @@ containerCompany.addEventListener("click", () => {
   companyMenu.classList.toggle("menu__display-none");
 });
 navListMenu.addEventListener("click", () => {
-  navListMenu.classList.toggle("");
+  sideBar.classList.remove("close__menu");
+  document.querySelector(".menu__bg").classList.remove("close__menu");
+});
+closeNav.addEventListener("click", () => {
+  document.querySelector(".menu__bg").classList.add("close__menu");
+  document.querySelector("#sideBar").classList.add("close__menu");
 });
